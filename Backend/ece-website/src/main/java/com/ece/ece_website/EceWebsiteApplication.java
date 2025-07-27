aa9e9c75f472219bee739a289eeea5f93eb32696
Damjan Stojchev <damjan.stojcev4@gmail.com>
2025-07-27 13:35:45 +0200

    Initialized Spring Boot

---
+++ Backend/ece-website/src/main/java/com/ece/ece_website/EceWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ece.ece_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EceWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EceWebsiteApplication.class, args);
+	}
+
+}
---
+++ Backend/ece-website/src/test/java/com/ece/ece_website/EceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ece.ece_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EceWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
