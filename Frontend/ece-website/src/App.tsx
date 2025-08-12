c02c350de90357f6b439fe51e51352050d06165c
Damjan Stojchev <damjan.stojcev4@gmail.com>
2025-08-12 20:18:48 +0200

    added route

--- Frontend/ece-website/src/App.tsx
+++ Frontend/ece-website/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar.tsx';
 import Home from './components/Home.tsx';
 import AboutUs from './components/AboutUs.tsx';
 import BackToTopButton from './components/BackToTopButton.tsx';
+import Contact from './components/Contact.tsx'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <BackToTopButton/>
     </Router>
