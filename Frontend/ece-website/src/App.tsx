d11f58d317d5ba6ae8aac7b462743080e92ed3f9
Damjan Stojchev <damjan.stojcev4@gmail.com>
2025-08-12 15:55:49 +0200

    Added Component

--- Frontend/ece-website/src/App.tsx
+++ Frontend/ece-website/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.tsx';
 import Home from './components/Home.tsx';
 import AboutUs from './components/AboutUs.tsx';
+import BackToTopButton from './components/BackToTopButton.tsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
       </Routes>
+      <BackToTopButton/>
     </Router>
   );
 }
