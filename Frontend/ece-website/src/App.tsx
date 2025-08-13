297c1ac078e382a1c24d4c999ba79e774fff3b09
Damjan Stojchev <damjan.stojcev4@gmail.com>
2025-08-13 14:29:56 +0200

    Added new Route and Scroll To Top

--- Frontend/ece-website/src/App.tsx
+++ Frontend/ece-website/src/App.tsx
@@ -4,15 +4,19 @@ import Home from './components/Home.tsx';
 import AboutUs from './components/AboutUs.tsx';
 import BackToTopButton from './components/BackToTopButton.tsx';
 import Contact from './components/Contact.tsx'
+import Admin from './components/Admin.tsx'
+import ScrollToTop from './components/ScrollToTop.tsx';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop/>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
       <BackToTopButton/>
     </Router>
