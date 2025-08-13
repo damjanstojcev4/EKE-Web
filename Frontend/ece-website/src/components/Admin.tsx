38345d50322368da2ce615457a3965cb3a53cfaf
Damjan Stojchev <damjan.stojcev4@gmail.com>
2025-08-13 14:39:45 +0200

    Admin component

---
+++ Frontend/ece-website/src/components/Admin.tsx
@@ -0,0 +1,14 @@
+const Vele = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-br from-cyan-900 to-cyan-600 text-white">
+      <div className="text-center space-y-4">
+        <h1 className="text-5xl font-bold">Hello from Admin👋</h1>
+        <p className="text-lg">
+          This is a hidden route. You can only see it if you know the link!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Vele;
