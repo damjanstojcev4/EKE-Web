cd4c1acb58ba1422d92c3f420496a94c86d849c9
Damjan Stojchev <damjan.stojcev4@gmail.com>
2025-08-29 10:47:14 +0200

    Modified method

--- Backend/ece-website/src/main/java/com/ece/ece_website/config/JwtAuthFilter.java
+++ Backend/ece-website/src/main/java/com/ece/ece_website/config/JwtAuthFilter.java
@@ -1,6 +1,7 @@
 package com.ece.ece_website.config;
 
 
+import com.ece.ece_website.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +15,9 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -21,5 +25,15 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
+        final String authHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String username;
+
+        if (authHeader == null && !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authHeader.substring(7);
+        username = jwtService.extactUsername(jwtToken);
     }
 }
