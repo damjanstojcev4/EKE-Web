14f8748660a3fa25f0945e4dac6e677bc2b86e39
Damjan Stojchev <damjan.stojcev4@gmail.com>
2025-11-03 13:02:23 +0100

    Responsive bugs

--- Frontend/ece-website/src/components/Home.tsx
+++ Frontend/ece-website/src/components/Home.tsx
@@ -19,17 +19,48 @@ import Partners from "./Partners";
 import RecentProjects from "./RecentProjects";
 import about from "../assets/about-us-bg.jpg";
 
-
 const Home: FC = () => {
   const aims = [
-    { text: "Personal and professional development", icon: BookOpen, color: "text-blue-600" },
-    { text: "Promoting mobility within the country and abroad", icon: Plane, color: "text-purple-600" },
-    { text: "Promote entrepreneurship", icon: Briefcase, color: "text-yellow-600" },
-    { text: "Digital literacy, digital skills and digital transformation", icon: Laptop, color: "text-indigo-600" },
-    { text: 'Sustainable development and the "green agenda"', icon: Leaf, color: "text-green-600" },
-    { text: "Promote a healthy lifestyle through sports and sports activities", icon: Dumbbell, color: "text-red-600" },
-    { text: "Advocate for human rights and empowerment of vulnerable and marginalized groups", icon: Users, color: "text-pink-600" },
-    { text: "Stand for democratic values and standards", icon: Scale, color: "text-gray-700" },
+    {
+      text: "Personal and professional development",
+      icon: BookOpen,
+      color: "text-blue-600",
+    },
+    {
+      text: "Promoting mobility within the country and abroad",
+      icon: Plane,
+      color: "text-purple-600",
+    },
+    {
+      text: "Promote entrepreneurship",
+      icon: Briefcase,
+      color: "text-yellow-600",
+    },
+    {
+      text: "Digital literacy, digital skills and digital transformation",
+      icon: Laptop,
+      color: "text-indigo-600",
+    },
+    {
+      text: 'Sustainable development and the "green agenda"',
+      icon: Leaf,
+      color: "text-green-600",
+    },
+    {
+      text: "Promote a healthy lifestyle through sports and sports activities",
+      icon: Dumbbell,
+      color: "text-red-600",
+    },
+    {
+      text: "Advocate for human rights and empowerment of vulnerable and marginalized groups",
+      icon: Users,
+      color: "text-pink-600",
+    },
+    {
+      text: "Stand for democratic values and standards",
+      icon: Scale,
+      color: "text-gray-700",
+    },
   ];
 
   return (
@@ -73,7 +104,7 @@ const Home: FC = () => {
       {/* Vision & Mission */}
       <section className="py-16">
         <div className="max-w-6xl mx-auto px-4 text-center">
-          <h2 className="text-5xl font-bold mb-12 text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-green-400">
+          <h2 className="text-4xl sm:text-5xl font-bold mb-12 text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-green-400 text-center whitespace-nowrap">
             Vision & Mission
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
@@ -111,27 +142,32 @@ const Home: FC = () => {
         <div className="max-w-6xl mx-auto px-6 grid md:grid-cols-2 gap-12 items-center">
           {/* Text */}
           <div className="space-y-6">
-            <h2 className="text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-600 to-blue-500">About Us</h2>
+            <h2 className="text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-600 to-blue-500">
+              About Us
+            </h2>
             <p className="text-gray-600 text-lg leading-relaxed">
               Since 2012, European Cultural Epicenter (EKE) has been fostering
               positive values and empowering youth through non-formal education,
               workshops, trainings, and international projects. Our mission is
               to connect communities and inspire sustainable change.
             </p>
-            <Link
-              to="/about-us"
-              className="inline-block px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow hover:bg-blue-500 transition"
-            >
-              Discover More
-            </Link>
-            <a
-              href="src/assets/PIF EKE Bitola 2025.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block px-6 py-3 ml-5 bg-green-800 text-white font-semibold rounded-lg shadow hover:bg-green-700 transition"
-            >
-              Become Partner
-            </a>
+            <div className="flex flex-col sm:flex-row sm:items-center gap-4 sm:gap-6 mt-4">
+              <Link
+                to="/about-us"
+                className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow hover:bg-blue-500 transition text-center"
+              >
+                Discover More
+              </Link>
+
+              <a
+                href="src/assets/PIF EKE Bitola 2025.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-6 py-3 bg-green-800 text-white font-semibold rounded-lg shadow hover:bg-green-700 transition text-center"
+              >
+                Become Partner
+              </a>
+            </div>
           </div>
 
           {/* Image / Illustration */}
@@ -156,20 +192,36 @@ const Home: FC = () => {
       <section>
         <div className="mt-20 mb-20 grid grid-cols-2 md:grid-cols-4 gap-8 max-w-4xl mx-auto">
           <div className="text-center">
-            <div className="text-3xl lg:text-4xl font-bold text-orange-500 mb-2">12+</div>
-            <div className="text-gray-600 text-sm font-medium">Years of Experience</div>
+            <div className="text-3xl lg:text-4xl font-bold text-orange-500 mb-2">
+              12+
+            </div>
+            <div className="text-gray-600 text-sm font-medium">
+              Years of Experience
+            </div>
           </div>
           <div className="text-center">
-            <div className="text-3xl lg:text-4xl font-bold text-green-600 mb-2">500+</div>
-            <div className="text-gray-600 text-sm font-medium">Participants Trained</div>
+            <div className="text-3xl lg:text-4xl font-bold text-green-600 mb-2">
+              500+
+            </div>
+            <div className="text-gray-600 text-sm font-medium">
+              Participants Trained
+            </div>
           </div>
           <div className="text-center">
-            <div className="text-3xl lg:text-4xl font-bold text-pink-700 mb-2">50+</div>
-            <div className="text-gray-600 text-sm font-medium">Successful Projects</div>
+            <div className="text-3xl lg:text-4xl font-bold text-pink-700 mb-2">
+              50+
+            </div>
+            <div className="text-gray-600 text-sm font-medium">
+              Successful Projects
+            </div>
           </div>
           <div className="text-center">
-            <div className="text-3xl lg:text-4xl font-bold text-blue-700 mb-2">15+</div>
-            <div className="text-gray-600 text-sm font-medium">Partner Organizations</div>
+            <div className="text-3xl lg:text-4xl font-bold text-blue-700 mb-2">
+              15+
+            </div>
+            <div className="text-gray-600 text-sm font-medium">
+              Partner Organizations
+            </div>
           </div>
         </div>
       </section>
@@ -181,8 +233,8 @@ const Home: FC = () => {
             Our Aims
           </h1>
           <p className="text-gray-600 mb-10 max-w-2xl mx-auto">
-            We are committed to achieving these key objectives through our programs
-            and initiatives.
+            We are committed to achieving these key objectives through our
+            programs and initiatives.
           </p>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-4xl mx-auto">
             {aims.map((aim, index) => {
